55cb458decef7378677054eeb43e85b4b4c200d1
lackita <lackita+github@gmail.com>
2011-08-05 08:30:46 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webcomics::Application.config.secret_token = '390e0ff0f1f1c62a3b12b8224d497c29026b8417bb7d9f0760c371dc7ce4824d8f145f9914a59b0fc1bd57d76482249cf6427fc00cf215b7a5b9b324f5a3304e'
